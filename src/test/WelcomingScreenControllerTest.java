850231c05a15fe7cd0f6bcb5e5ede0e6a39d5219
Hyuntaek Lim <ryanlim1205@gmail.com>
2021-12-28 01:11:04 +0900

    Added JUnit tests

---
+++ src/test/WelcomingScreenControllerTest.java
@@ -0,0 +1,23 @@
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.control.Label;
+import org.junit.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+class WelcomingScreenControllerTest {
+
+    @Test
+    public void test_onStartButtonClick() {
+        clickOn("");
+    }
+
+    @Test
+    void onOptionsButtonClick() {
+    }
+
+    @Test
+    void onExitButtonClick() {
+    }
+}
